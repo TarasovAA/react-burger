c089a9c0efdf3e424cb9d88a4869143b20294550
Andrew Tarasov <tarasovaa1997@bk.ru>
2024-02-08 08:36:50 +0300

    sprint2/step2 added counter for ingridients

--- src/components/burger-ingredients/ingredient.js
+++ src/components/burger-ingredients/ingredient.js
@@ -1,22 +1,22 @@
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import { IngredientsDataType } from '../../utils/data';
 import styles from './burger-ingredients.module.css';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {SET_VIEWED_INGREDIANT_ITEM} from '../../services/actions';
 import { useDrag } from 'react-dnd'
 
 const Ingredient = ({ item }) => {
   const dispatch = useDispatch();
 
-  const setItemToModal = () => {
-    dispatch({
-      type: SET_VIEWED_INGREDIANT_ITEM,
-      payload: item
-    });
-  
-  }
-
+  const burgerConstructor = useSelector(store => store.burgerConstructor)
   const {_id, type} = item;
+
+  const count = (type === 'bun') ? 
+                  burgerConstructor.head.filter(_ => _._id === item._id).length
+                  :
+                  burgerConstructor.body.filter(_ => _._id === item._id).length;
+
+  
   
   const [{isDrag}, dragRef] = useDrag({
     type: (type === 'bun') ? 'bun': 'ingredient',
@@ -26,11 +26,23 @@ const Ingredient = ({ item }) => {
     })
   });
   
+  const opacity = isDrag ? 0.2 : 1;
+
+  const setItemToModal = () => {
+    dispatch({
+      type: SET_VIEWED_INGREDIANT_ITEM,
+      payload: item
+    });
+  
+  }
+
   return ( 
-    !isDrag && <div ref={dragRef}
+    <div ref={dragRef}
             className={styles.ingredientForm}
             onClick={setItemToModal}
+            style={{opacity}}
   >
+    {count !== 0 && <Counter count={count} size="default" extraClass="m-1" />}
     <img src={item.image} alt={item.name} />
     <div className={`${styles.textCenteror} mt-2`}>
       <p className="text text_type_main-medium">{item.price}<CurrencyIcon /></p>
