fa2a41a986d8ca86347a6c5572790916109de43f
Andrew Tarasov <tarasovaa1997@bk.ru>
2024-05-12 13:41:52 +0300

    sprint-5-step2 added memorization in GetIngredientsByIds for removing extra warnings

--- src/services/ingredients/selectors.ts
+++ src/services/ingredients/selectors.ts
@@ -1,5 +1,6 @@
 import { useSelector } from "../hooks";
 import { RootState } from "../types";
+import { createSelector } from "@reduxjs/toolkit";
 
 export const GetAllIngredientsRequestData = ()  => {
     return useSelector((store: RootState) => store.allIngredients);
@@ -9,6 +10,10 @@ export const GetAllIngredients = ()  => {
     return useSelector((store: RootState) => store.allIngredients.allIngredients);
 }
 
-export const GetIngredientsByIds = (ids: Array<string>) => {
-    return useSelector((store: RootState) => store.allIngredients.allIngredients.filter((i: any) => ids.includes(i._id)));
+const allIngredients = (store: RootState) => store.allIngredients.allIngredients;
+
+export const GetMemoFilteredIngredients = createSelector([allIngredients, (allIngredients, ids: Array<string>)=>ids], (allIngredients, ids) => allIngredients.filter((i: any) => ids.includes(i._id)))
+
+export const GetIngredientsByIds = (ingredients: Array<string>) => {
+    return useSelector((store: RootState) => GetMemoFilteredIngredients(store, ingredients));
 }
