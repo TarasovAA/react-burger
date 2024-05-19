2bf696d97a1dcb3e96023f41b51f387f6e14ec41
Andrew Tarasov <tarasovaa1997@bk.ru>
2024-05-19 03:50:41 +0300

    sprint-6/step-2 set basic settings for cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,8 +4,28 @@
 //   })
 // })
 
+import {data} from '../../src/utils/data'
+
 describe('Burger constructor', () => {
   beforeAll(() => {
     cy.visit('/');
+
+    cy.intercept('GET', 'https://norma.nomoreparties.space/api/auth/user', {fixture: "user.json"})
+    cy.intercept('GET', 'https://norma.nomoreparties.space/api/ingredients',  {fixture: "ingredoents.json"})
+
+    window.localStorage.setItem(
+      "refreshToken",
+      JSON.stringify("test-refreshToken")
+    )
+  
+    window.localStorage.setItem(
+      "accessToken",
+      JSON.stringify("test-accessToken")
+    )
+  })
+
+  afterEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
   })
 })
