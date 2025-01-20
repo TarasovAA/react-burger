ad282e88b42278e2db6beb36a6c58a2a50247123
Andrew Tarasov <tarasovaa1997@bk.ru>
2025-01-20 15:35:49 +0300

    добавил локальную серверную часть

---
+++ server/Controllers/IngredientsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        [HttpGet()]
+        public IEnumerable<Ingredient> Get()
+        {
+            using StreamReader r = new StreamReader("file.json");
+
+            string json = r.ReadToEnd();
+            List<Ingredient> items = JsonConvert.DeserializeObject<List<Ingredient>>(json);
+
+            return items;
+        }
+    }
+}
---
+++ server/Ingredient.cs
@@ -0,0 +1,4 @@
+﻿namespace server;
+
+public record Ingredient(string _id, string name, string type, int proteins, int fat, int carbohydrates, int calories, int price, string image, string image_mobile, string image_large);
+
